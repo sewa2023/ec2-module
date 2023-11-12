d2bbc352e43a0b0d17d3a2ca945bd658fc3c6480
sewa2023 <sewasewavi@yahoo.com>
2023-11-11 20:56:55 -0600

    newcode

---
+++ variables.tf
@@ -0,0 +1,14 @@
+ variable "ami" {
+  description = "Amazon machine image"
+  type = string
+  default = "ami-0e8a34246278c21e4"
+ }
+ variable "region" {
+   default = "us-east-1"
+ }
+ variable "instance_type" {
+   default = "t3.small"
+ }
+ variable "name" {
+   default = "Dev-sewa"
+ }
